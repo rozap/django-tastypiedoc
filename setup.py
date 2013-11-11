fb51648af0b8cc9af97b18a8dca6cb958e86641d
rozap <chrisd1891@gmail.com>
2013-11-11 14:46:17 -0800

    pypi version bump?

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-tastypiedoc',
-    version='0.2.1',
+    version='0.2.2',
     packages=['tastypiedoc'],
     include_package_data=True,
     license='MIT License',  # example license
